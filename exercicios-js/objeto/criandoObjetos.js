a0de67d2e53a2bfba535418bca1aee2502dc04a2
Reinaldo P Jr <reinaldo800@gmail.com>
2019-02-05 21:41:46 -0200

    Seção 6 , Aula 89 - Objetos Constantes

---
+++ exercicios-js/objeto/criandoObjetos.js
@@ -0,0 +1,48 @@
+// usando a notação literal
+const obj1 = {}
+console.log(obj1)
+
+
+//Object em JS
+console.log(typeof Object, typeof new Object)  // Objeto foi criado a partir de uma função construtora
+const obj2 =  new Object
+console.log(obj2)
+
+// Funções construtoras
+function Produto(nome, preco, desconto) {
+    this.nome = nome    // está publica e não esta encapsulada dentro do objeto
+    this.getPrecoComDesconto = () => {          //metodo
+        return preco * (1 - desconto)           //encapsulado
+    }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998.99, 0.25)
+console.log(p1.nome+ ': ' + p1.getPrecoComDesconto(), p2.nome + ': ' + p2.getPrecoComDesconto())
+
+// Função Factory (padrão de projeto)
+
+function criarFuncionario(nome, salarioBase, faltas){
+    return {                                            // objeto literal
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (salarioBase / 30) * (30 - faltas)           // quanto ele ganha por dia // quanto ele ganhou tirando as faltas
+        }
+    }
+}
+
+const f1 = criarFuncionario('João', 7980, 4)
+const f2 = criarFuncionario('Maria', 11400, 1)
+console.log(f1.nome + ': ' + f1.getSalario(), f2.nome + ': ' + f2.getSalario())
+
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+// Uma função famosa que retorna um Objeto...
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)  //  converteu esse texto em um objeto com uma variável
