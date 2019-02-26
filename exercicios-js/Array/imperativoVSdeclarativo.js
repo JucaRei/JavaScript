cdf1cc13e76107e849b338a0bc57b974842023a8
Reinaldo Ponce Junior <macjr@Reinaldos-MacBook-Pro.local>
2019-02-26 17:45:05 -0300

    Modulo node start

--- exercicios-js/Array/imperativoVSdeclarativo.js
+++ exercicios-js/Array/imperativoVSdeclarativo.js
@@ -7,6 +7,14 @@ const alunos = [
 let total1 = 0    // acumular os valores
 for (let i = 0; i < alunos.length; i++) {
     total1 += alunos[i].nota     //percorre cada 1 dos alunos pegando o indice, a nota esta acrescentando so total1
-}
+}   //tem menos reuso
 
 console.log(total1 / alunos.length)
+
+// Declarativo
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+//esse metodos podem ser reusados
+
+const total2 = alunos.map(getNota).reduce(soma)         // reduce tem a ideia de reduzir , agregar
+console.log(total2 / alunos.length)
