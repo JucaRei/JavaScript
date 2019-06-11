4f56267fec1e309e1ee57c78d2fe5e1a0c564070
Juca <reinaldo800@gmail.com>
2019-06-10 21:54:04 -0300

    Gulp

--- exercicios-js/ESNext/map.js
+++ exercicios-js/ESNext/map.js
@@ -1,18 +1,18 @@
 // Map ( estrutura chave-valor que não aceita repetição na chave)
 const tecnologias = new Map()
-tecnologias.set('react', { framework: false})
-tecnologias.set('angular', {framework: true})
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
 console.log(tecnologias.react)  // jeito errado de acessar o map Tecnologias
 console.log(tecnologias.get('react'))   // jeito correto
-console.log(tecnologias.get('react').framework)   
+console.log(tecnologias.get('react').framework)
 
-const chavesVariadas = new Map ([    //array
-    [function() { }, 'Função' ],     // função como chave e o valor é um texto
+const chavesVariadas = new Map([    //array
+    [function () { }, 'Função'],     // função como chave e o valor é um texto
     [{}, 'Objeto'],    // objeto com chave , valor string
     [123, 'Número']    // chave é um numero , valor string
 ])
 
-chavesVariadas.forEach((vl, ch)  => {    // acessa primeiro o valor e depois a chave
+chavesVariadas.forEach((vl, ch) => {    // acessa primeiro o valor e depois a chave
     console.log(ch, vl)
 })
 
