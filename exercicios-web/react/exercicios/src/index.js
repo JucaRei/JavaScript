b3ce95e29a50daf93a39164718090943ff9fb557
Juca <reinaldo800@gmail.com>
2019-07-07 15:54:00 -0300

    Travando

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,4 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>React 2</h1>, document.getElementById('root'))
+import Primeiro from "./componentes/Primeiro";
+
+const elemento = <h1>React 2</h1>;
+
+ReactDOM.render(<Primeiro />, document.getElementById("root"));
